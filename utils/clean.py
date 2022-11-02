09f9d598e3d687d3c003875c5f578043083dc7fd
Alejandro Pachas <alejandromydev@gmail.com>
2022-11-02 18:00:05 -0500

    clean file funcionality were split into diferent files

--- utils/clean.py
+++ utils/clean.py
@@ -1,21 +1,28 @@
 import pandas as pd
+import pycountry_convert as pc
+from utils.addContinent import handle as addContinent
+from utils.resetIndex import handle as resetIndex 
 
-def handle(df):
+def handle(df:pd):
   
   # eliminamos la columna country para ser remplazada por otra
-  df =df.drop(columns=['country'])
+  df = df.drop(columns=['country'])
 
   # renombramos el atributo "iso3" que trae la variable df, por "acronym_country" y "admin_name" por region 
-  df= df.rename(columns={'iso2':'country','admin_name':'region'})
+  df = df.rename(columns={'iso2':'country','admin_name':'region'})
   #borramos los valores nulos de la poblacion y de la capital
-  df=df.dropna(subset=['population'])
-  df=df.dropna(subset=['capital'])
+  df = df.dropna(subset=['population'])
+  df = df.dropna(subset=['capital'])
 
   #pasamos la poblacion a entero
   df.population=df.population.astype(int)
   df.isnull().sum()
-  # vuelve asignar un index a cada país
-  df=df.groupby(['city','region','country','lng','lat','population'])['capital'].count().reset_index()
-  df =df.drop(columns=['capital'])
+  df = resetIndex(df)
+  [df,continent] = addContinent(df)
+  df = df.drop(columns=['capital'])
+  df.insert(6 ,"continent",continent)
+
 
   return df
+
+
